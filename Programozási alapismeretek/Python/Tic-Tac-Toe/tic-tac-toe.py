7f594dc0f454f9f8cc139333e56c144f14f7e5b3
Almási Gergely <116632531+AlmasiGergelyMechwart@users.noreply.github.com>
2023-10-08 14:35:06 +0200

    Update tic-tac-toe.py
    2023.10.08

--- Programozási alapismeretek/Python/Tic-Tac-Toe/tic-tac-toe.py
+++ Programozási alapismeretek/Python/Tic-Tac-Toe/tic-tac-toe.py
@@ -1,27 +1,79 @@
-sor = 3
-oszlop = 3
-# board = [" "*oszlop]*sor #ez nem jó mert nem tudom
-board = [" "," "," "],[" "," "," "],[" "," "," "]
+row = 3
+col = 3
+board = []
+for i in range(row):
+    board.append([])
+    for j in range(col):
+        board[i].append(" ")
+
 players = ["X", "O"]
 
 def drawBoard():
-    for i in range(sor):
-        for j in range(oszlop):
+    for i in range(row):
+        for j in range(col):
             print(f"[{board[i][j]}]",end="")
         print()
 
-# def winTest():
-#     for i in range(sor)
+def winTest():
+    dontB = True
+    for i in range(row):
+        if dontB == False:
+            break
+        for j in range(col):
+            if board[i][j] == " ":
+                dontB = False
+                break
+    if dontB:
+        return "D"
+
+    for a in range(row):
+        if board[a][a] != " ":
+            temp = board[a][a]
+        else:
+            continue
+
+        for i in range(row): # oszlop
+            if temp != board[i][a]:
+                break
+        else:
+            return temp
+        
+        for i in range(col): # sor
+            if temp != board[a][i]:
+                break
+        else:
+            return temp
+        
+    if row != col:
+        return
+        
+    if board[0][0] != " ":
+        temp = board[0][0]
+        for i in range(1, row): # átló (bal fent - jobb lent)
+            if temp != board[i][i]:
+                break
+        else:
+            return temp
+    
+    if board[row-1][0] != " ":
+        temp = board[row-1][0]
+        for i in range(1, row): # átló (bal lent - jobb fent)
+            if temp != board[row-1-i][i]:
+                break
+        else:
+            return temp
 
 def game():
-    gameIsOn = True
     playerPos = [0, 0]
     currentPlayer = 0
-    while gameIsOn:
-        drawBoard()
 
-        inputLoop = True
-        while inputLoop:
+    if row != col:
+        print("Nem négyzet alakú pályán átlós nyereség nem lehetséges")
+
+    while True:
+        while True:
+            drawBoard()
+
             print(f"{players[currentPlayer]} jön")
             playerPos[0] = input("sor: ")
             playerPos[1] = input("oszlop: ")
@@ -31,7 +83,7 @@ def game():
             except:
                 print("A megadott érték nem egy egész szám")
                 continue
-            if (playerPos[0] < 1 or playerPos[0] > sor) or (playerPos[1] < 1 or playerPos[1] > oszlop):
+            if (playerPos[0] < 1 or playerPos[0] > row) or (playerPos[1] < 1 or playerPos[1] > col):
                 print("A megadott koordináta a pályán kívül esik")
                 continue
             else:
@@ -39,11 +91,18 @@ def game():
                 playerPos[1]-=1
             if board[playerPos[0]][playerPos[1]] == " ":
                 board[playerPos[0]][playerPos[1]] = players[currentPlayer]
-                inputLoop = False
+                break
             else:
                 print("A meghatározott terület már foglalt")
 
-        # winTest()
+        winner = winTest()
+        if winner:
+            drawBoard()
+            if winner == "D":
+                print("Döntetlen")
+            else:
+                print(f"{winner} nyert")
+            break
 
         if currentPlayer == 0:
             currentPlayer = 1
