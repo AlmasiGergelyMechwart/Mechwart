952ab368685ccd7e31572dd59f9a8aac9df2a017
Almási Gergely <116632531+AlmasiGergelyMechwart@users.noreply.github.com>
2024-02-15 14:08:29 +0100

    2024.02.15

---
+++ Programozási alapismeretek/Python/20240215/sample.py
@@ -0,0 +1,46 @@
+# Írj egy függvényt, ami megkap egy listát (pl.: [1,2,3,4]) és visszaadja a kumulatív összeget (pl.: 1, 1+2, 1+2+3 ..., tehát az előző példa a [1, 3, 6, 10] listát kell hogy visszaadja). A függvény működjön tetszőleges, számokat tartalmazó listákkal!
+
+def kumulativ(li):
+	#TODO...
+	return
+
+
+# Írj függvényt, amely megfordít egy megkapott listát, és azt visszaadja!
+
+def reverse(li):
+	#TODO...
+	return
+
+
+# Írj függvényt, ami a következőt hajtja végre: 
+# Ha az átadott sztring hossza legalább 3, akkor a végéhez adjuk hozzá az 'ing' ragot.
+# Ha 'ing'-re végződik, akkor ehelyett az 'ly' ragot tegyük hozzá.
+# Ha a sztring hossza rövidebb 3 karakternél, akkor hagyjuk változatlanul.
+# Adjuk vissza az eredménysztringet.
+
+def verbing(s):
+	# TODO...
+	return
+	
+# Egy adott sztringben keressük meg a 'not' és 'bad' szavak előfordulási helyét. Ha a 'bad' a 'not' szót követi, akkor cseréljük ki az egész 'not'...'bad' részsztringet a 'good' szóra.
+# Adjuk vissza az eredmény sztringet.
+# Példa: 'This dinner is not that bad!' ->
+#        This dinner is good!
+
+def not_bad(s):
+    # TODO...
+    return
+
+# Írj függvényt, amely eldönti egy szóról, hogy az palindróm-e (visszafelé ugyan az, mint előre felé)! A függvény igaz-hamis értékkel térjen vissza!
+
+def palindrom(szo):
+	# TODO...
+	return
+	
+# Döntsük el egy magyar szóról, hogy milyen hangrendű (mély, magas, vegyes, vagy semmilyen hangrendű).
+# Ha egy szóban vmennyi magánhangzó mély hangrendű, akkor a szó mély hangrendű. Ha egy szóban vmennyi magánhangzó magas hangrendű, akkor a szó magas hangrendű. Ha egy szóban mély és magas hangrendű magánhangzók is előfordulnak, akkor a szó vegyes hangrendű. Ha a szóban nincs egyetlen magánhangzó sem, akkor semmilyen hangrendű.
+
+# Mély hangrendű magánhangzók: a, á, o, ó, u, ú.
+# Magas hangrendű magánhangzók: e, é, i, í, ö, ő, ü, ű. 
+
+
