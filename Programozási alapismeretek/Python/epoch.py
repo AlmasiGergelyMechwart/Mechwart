7c125df8d5a79ea8b1667fbbd5157dd66bbad8c3
Almási Gergely <116632531+AlmasiGergelyMechwart@users.noreply.github.com>
2024-04-18 14:42:14 +0200

    Add files via upload

--- Programozási alapismeretek/Python/epoch.py
+++ Programozási alapismeretek/Python/epoch.py
@@ -12,39 +12,39 @@ class Date():
 
     def toList(self):
         return (self.year, self.month, self.day, self.hour, self.minute, self.second)
-    
+
     def toStr(self, style="basic", format="YMD", sep="/"):
-    	dateDict = {"Y": str(self.year), "M": str(self.month), "D": str(self.day)}
-    	date = []
-    	for c in format:
-    		date.append(dateDict[c.upper()])
-    	date = sep.join(date)
-    	time = ":".join((str(self.hour), str(self.minute)))
-    	longTime = ":".join((time, str(self.second)))
-    	
-    	if style == "basic":
-    		return "%s %s" % (date, time)
-    	if style == "all":
-    		return "%s %s" % (date, longTime)
-    	if style == "date":
-    		return date
-    	if style == "time":
-    		return time
-    	if style == "longTime":
-    		return longTime
-    
+        dateDict = {"Y": str(self.year), "M": str(self.month), "D": str(self.day)}
+        date = []
+        for c in format:
+            date.append(dateDict[c.upper()])
+        date = sep.join(date)
+        time = ":".join((str(self.hour), str(self.minute)))
+        longTime = ":".join((time, str(self.second)))
+
+        if style == "basic":
+            return "%s %s" % (date, time)
+        if style == "all":
+            return "%s %s" % (date, longTime)
+        if style == "date":
+            return date
+        if style == "time":
+            return time
+        if style == "longTime":
+            return longTime
+
     def toInt(self):
         leapDays = (self.year-1969)//4 - (self.year-1901)//100 + (self.year-1601)//400
         daysInMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         daysFromMonths = sum(daysInMonth[:self.month])
-        if self.month > 2 and (self.year%4==0 and self.year%100!=0 and self.year%400==0):
+        if self.month > 2 and ((self.year%4==0 and self.year%100!=0) or self.year%400==0):
             daysFromMonths+=1
             print("leap year")
         return ((((self.year-1970)*365 + leapDays + daysFromMonths + self.day-1) * 24 + self.hour) * 60 + self.minute) * 60
 
     def toIntDatetime(self):
         return int(datetime(*self.toList()).timestamp())
-    
+
     def to_seconds_since_epoch(self):
         days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         leap_days = (self.year - 1969) // 4 - (self.year - 1901) // 100 + (self.year - 1601) // 400
@@ -66,6 +66,7 @@ class Date():
 
 for i in range(10):
     time = Date(random.randint(1970, 2024), random.randint(1, 12), random.randint(1, 31), random.randint(0, 23), random.randint(0, 59), random.randint(0, 59))
+    time.year = time.year//4*4
     print()
     print(time.toStr(style="all"))
     print(time.toIntDatetime())
